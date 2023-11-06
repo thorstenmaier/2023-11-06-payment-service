45c36bd00325ef9718b2f97df10673b768e92e05
Thorsten Maier <thorsten.a.maier@accenture.com>
2023-11-06 12:27:31 +0100

    Use http status codes as response in cause of error

--- src/main/java/com/example/paymentservice/CustomerPaymentController.java
+++ src/main/java/com/example/paymentservice/CustomerPaymentController.java
@@ -1,17 +1,29 @@
 package com.example.paymentservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Collection;
 
 @RestController
 public class CustomerPaymentController {
 
+    private final CustomerPaymentMethodRepository customerPaymentMethodRepository;
+
+    public CustomerPaymentController(CustomerPaymentMethodRepository customerPaymentMethodRepository) {
+        this.customerPaymentMethodRepository = customerPaymentMethodRepository;
+    }
+
     @GetMapping("customerPaymentMethod/{id}")
-    public CustomerPaymentMethodResource getCustomerPaymentMethod(@PathVariable("id") Long customerId) {
-        // TODO Access database
-        return new CustomerPaymentMethodResource(List.of(PaymentMethod.PAYPAL));
+    public ResponseEntity<CustomerPaymentMethodResource> getCustomerPaymentMethod(@PathVariable("id") Long customerId) {
+        Collection<CustomerPaymentMethod> customerPaymentMethods = customerPaymentMethodRepository.findByCustomerId(customerId);
+
+        if (customerPaymentMethods.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(new CustomerPaymentMethodResource(customerPaymentMethods.stream().map(CustomerPaymentMethod::getPaymentMethod).toList()));
     }
 }
--- src/main/java/com/example/paymentservice/CustomerPaymentMethodRepository.java
+++ src/main/java/com/example/paymentservice/CustomerPaymentMethodRepository.java
@@ -2,5 +2,8 @@ package com.example.paymentservice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface CustomerPaymentMethodRepository extends JpaRepository<CustomerPaymentMethod, Long> {
+    Collection<CustomerPaymentMethod> findByCustomerId(Long customerId);
 }
